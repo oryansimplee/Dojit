3343056e39e641b517140d34831a6e804a22b3e2
oryan <oryan@simplee.com>
2016-04-18 14:58:22 +0300

    from group using string instead of html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def form_group_tag(errors, &block)
+  def form_group_tag(errors)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+       'form-group has-error'
     else
-      content_tag :div, capture(&block), class: 'form-group'
+       'form-group'
     end
   end
 end
