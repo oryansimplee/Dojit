9e7999cbd481ffca7a2ddc65399af08ac8c179d7
oryan <oryan@simplee.com>
2016-04-25 10:46:26 +0300

    interlude vote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,12 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+
+  def vote_link_classes(post, direction)
+    if(direction=='up')
+      "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }" 
+    else
+      "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+    end
+  end
 end
